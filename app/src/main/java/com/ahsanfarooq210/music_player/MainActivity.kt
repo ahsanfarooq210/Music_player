fcf7564adbfdb9fc934ff132f46191e79c29c471
ahsan <ahsanfarooq210@gmail.com>
2022-07-16 23:17:57 +0500

    18.Runtime Permission in Android Studio

--- app/src/main/java/com/ahsanfarooq210/music_player/MainActivity.kt
+++ app/src/main/java/com/ahsanfarooq210/music_player/MainActivity.kt
@@ -1,24 +1,31 @@
 package com.ahsanfarooq210.music_player
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import com.ahsanfarooq210.music_player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
     private lateinit var binding:ActivityMainBinding
+    private lateinit var storagePermission:ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
+        requestRuntimePermission()
         binding=ActivityMainBinding.inflate(layoutInflater)
         setTheme(R.style.Theme_Music_player)
         setContentView(binding.root)
 
 
         initViews()
+
 
     }
 
@@ -38,6 +45,37 @@ class MainActivity : AppCompatActivity()
         binding.favoutireBtn.setOnClickListener {
             val intent= Intent(this@MainActivity,FavouriteActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    /**
+     * for requesting permisssions
+     * */
+    private fun requestRuntimePermission()
+    {
+        if(ActivityCompat.checkSelfPermission(this@MainActivity,android.Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED)
+        {
+            Toast.makeText(this@MainActivity, "denied", Toast.LENGTH_SHORT).show()
+            initiatePermissionContract()
+            storagePermission.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+    }
+
+    /**
+     * function to initiate the permission contracts
+     * */
+    private fun initiatePermissionContract()
+    {
+        storagePermission=registerForActivityResult(ActivityResultContracts.RequestPermission())
+        {
+            if(it)
+            {
+                Toast.makeText(this@MainActivity, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                Toast.makeText(this@MainActivity, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
