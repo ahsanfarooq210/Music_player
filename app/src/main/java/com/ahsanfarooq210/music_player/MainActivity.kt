a327db28093eb9ab10b1f786e571f1e251f7f4c9
ahsan <ahsanfarooq210@gmail.com>
2022-07-16 21:02:48 +0500

    15.Binding in Android Studio

--- app/src/main/java/com/ahsanfarooq210/music_player/MainActivity.kt
+++ app/src/main/java/com/ahsanfarooq210/music_player/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ahsanfarooq210.music_player
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.ahsanfarooq210.music_player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
@@ -15,6 +16,14 @@ class MainActivity : AppCompatActivity()
         setTheme(R.style.Theme_Music_player)
         setContentView(binding.root)
 
+        initViews()
 
+    }
+
+    private fun initViews()
+    {
+        binding.shuffleBtn.setOnClickListener {
+            Toast.makeText(this@MainActivity,"button clicked ",Toast.LENGTH_SHORT).show()
+        }
     }
 }
