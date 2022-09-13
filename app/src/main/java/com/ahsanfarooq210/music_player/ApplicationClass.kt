c3108caee80b622e9212c1dab33636c7a5a0f718
Muhammad Ahsan Farooq <ahsanfarooq210@gmail.com>
2022-09-13 22:50:11 +0500

    33.Creating ApplicationClass

---
+++ app/src/main/java/com/ahsanfarooq210/music_player/ApplicationClass.kt
@@ -0,0 +1,30 @@
+package com.ahsanfarooq210.music_player
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+
+class ApplicationClass:Application()
+{
+    companion object
+    {
+        const val CHANNEL_ID="channel1"
+        const val PLAY="play"
+        const val NEXT="next"
+        const val previous="previous"
+        const val EXIT="exit"
+    }
+
+    override fun onCreate()
+    {
+        super.onCreate()
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O)
+        {
+            val notificationChannel=NotificationChannel(CHANNEL_ID,"Now Playing Song",NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.description="This is a Important channel for showing songs"
+            val notificationManager=getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+}
